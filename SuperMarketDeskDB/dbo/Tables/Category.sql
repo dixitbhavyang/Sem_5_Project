be96122a35b7f00f393fe4eaac34cdd388209200
DixitBhavyang <dixitbhavyang111@gmail.com>
2023-07-07 17:30:01 +0530

    Update Category.sql

--- SuperMarketDeskDB/dbo/Tables/Category.sql
+++ SuperMarketDeskDB/dbo/Tables/Category.sql
@@ -1,5 +1,5 @@
 ﻿CREATE TABLE [dbo].[Category] (
-    [Id]          INT          NOT NULL,
+    [Id]          INT          IDENTITY (1, 1) NOT NULL,
     [CompanyId]   INT          NULL,
     [Name]        VARCHAR (20) NULL,
     [ShortName]   VARCHAR (20) NULL,
@@ -7,9 +7,14 @@
     [CreatedBy]   INT          NULL,
     [UpdatedDate] DATETIME     NULL,
     [Updatedby]   INT          NULL,
-    [Status]      BIT          NULL,
-    CONSTRAINT [PK_Category] PRIMARY KEY CLUSTERED ([Id] ASC)
+    [Status]      BIT          CONSTRAINT [DF_Category_Status] DEFAULT ((1)) NULL,
+    CONSTRAINT [PK_Category] PRIMARY KEY CLUSTERED ([Id] ASC),
+    FOREIGN KEY ([CompanyId]) REFERENCES [dbo].[Company] ([Id]),
+    FOREIGN KEY ([CreatedBy]) REFERENCES [dbo].[Users] ([Id]),
+    FOREIGN KEY ([Updatedby]) REFERENCES [dbo].[Users] ([Id])
 );
+
+
 
 
 
