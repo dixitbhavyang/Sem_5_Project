34683490b9bd030ccc5f0e5709f56a7d7dc9e4c9
DixitBhavyang <dixitbhavyang111@gmail.com>
2023-07-07 17:29:29 +0530

    Create DELETE_CATEGORY.sql

---
+++ SuperMarketDeskDB/dbo/Stored Procedures/DELETE_CATEGORY.sql
@@ -0,0 +1,18 @@
+﻿CREATE PROCEDURE [dbo].[DELETE_CATEGORY]
+    @ID INT
+AS
+BEGIN
+    SET NOCOUNT ON;
+
+    -- Check if the record exists
+    IF EXISTS (SELECT * FROM Item WHERE CategoryId = @ID)
+    BEGIN
+        -- Return the existing record
+        SELECT NAME FROM Item WHERE CategoryId = @ID;
+    END
+    ELSE
+    BEGIN
+        -- Update the record with the new value
+        UPDATE Category SET [Status] = 0 WHERE ID = @ID;
+    END
+END
