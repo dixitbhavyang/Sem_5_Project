79ce95a9bda44e868c079a0e0af3edfdae4b76d5
DixitBhavyang <dixitbhavyang111@gmail.com>
2023-07-07 17:29:37 +0530

    Update DELETE_COMPANY.sql

--- SuperMarketDeskDB/dbo/Stored Procedures/DELETE_COMPANY.sql
+++ SuperMarketDeskDB/dbo/Stored Procedures/DELETE_COMPANY.sql
@@ -1,5 +1,18 @@
-﻿CREATE PROC DELETE_COMPANY @ID INT
-AS 
-BEGIN 
-UPDATE Company SET [Status]=0 WHERE Id=@ID;
+﻿CREATE PROCEDURE DELETE_COMPANY
+    @ID INT
+AS
+BEGIN
+    SET NOCOUNT ON;
+
+    -- Check if the record exists
+    IF EXISTS (SELECT * FROM Category WHERE CompanyId = @ID)
+    BEGIN
+        -- Return the existing record
+        SELECT NAME FROM Category WHERE CompanyId = @ID;
+    END
+    ELSE
+    BEGIN
+        -- Update the record with the new value
+        UPDATE Company SET [Status] = 0 WHERE ID = @ID;
+    END
 END
