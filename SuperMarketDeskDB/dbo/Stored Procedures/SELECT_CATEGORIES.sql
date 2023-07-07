1e8347d607b61797c1619e317b9474857ad71576
DixitBhavyang <dixitbhavyang111@gmail.com>
2023-07-07 17:29:50 +0530

    Create SELECT_CATEGORIES.sql

---
+++ SuperMarketDeskDB/dbo/Stored Procedures/SELECT_CATEGORIES.sql
@@ -0,0 +1,10 @@
+﻿CREATE PROC [dbo].[SELECT_CATEGORIES]
+AS
+BEGIN
+SELECT  C.Id, C.[Name], C.ShortName, C.CreatedDate, U.Username "Created By", C.UpdatedDate, U2.Username "Updated By",C2.[Name] "Company", C.[Status]
+FROM Category "C"
+LEFT JOIN Users "U" ON C.CreatedBy = U.Id
+LEFT JOIN Users "U2" ON C.UpdatedBy = U2.Id 
+LEFT JOIN Company "C2" ON C.CompanyId = C2.Id
+WHERE C.[Status]=1
+END
