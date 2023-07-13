ce9ffe0a46e94faf4361f88270a20d64dad274b6
DixitBhavyang <dixitbhavyang111@gmail.com>
2023-07-13 22:23:03 +0530

    UPDATED

--- SuperMarketDeskDB/dbo/Tables/Item.sql
+++ SuperMarketDeskDB/dbo/Tables/Item.sql
@@ -1,17 +1,24 @@
 ﻿CREATE TABLE [dbo].[Item] (
-    [Id]          NCHAR (10)     NOT NULL,
-    [CategoryId]  INT            NULL,
+    [Id]          INT            IDENTITY (1, 1) NOT NULL,
+    [CategoryId]  INT            NOT NULL,
     [Name]        VARCHAR (100)  NULL,
     [ShortName]   VARCHAR (10)   NULL,
     [Price]       DECIMAL (9, 2) NULL,
     [Discount]    DECIMAL (9, 2) NULL,
     [Tax]         DECIMAL (9, 2) NULL,
     [CreatedDate] DATETIME       NULL,
+    [CreatedBy]   INT            NULL,
     [UpdatedDate] DATETIME       NULL,
+    [UpdatedBy]   INT            NULL,
     [Status]      BIT            CONSTRAINT [DF_Item_Status] DEFAULT ((1)) NULL,
     [ExpiryDate]  DATETIME       NULL,
-    CONSTRAINT [PK_Item] PRIMARY KEY CLUSTERED ([Id] ASC)
+    CONSTRAINT [PK_Item] PRIMARY KEY CLUSTERED ([Id] ASC),
+    FOREIGN KEY ([CreatedBy]) REFERENCES [dbo].[Users] ([Id]),
+    FOREIGN KEY ([UpdatedBy]) REFERENCES [dbo].[Users] ([Id]),
+    CONSTRAINT [FK__Item__CategoryId__3864608B] FOREIGN KEY ([CategoryId]) REFERENCES [dbo].[Category] ([Id])
 );
+
+
 
 
 
