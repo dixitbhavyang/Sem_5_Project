c2aaa500e297e27f3ae123ad780def257155eef9
DixitBhavyang <dixitbhavyang111@gmail.com>
2023-07-13 22:21:41 +0530

    Create SELECT_ACTIVE_CATEGORIES.sql

---
+++ SuperMarketDeskDB/dbo/Stored Procedures/SELECT_ACTIVE_CATEGORIES.sql
@@ -0,0 +1,8 @@
+﻿CREATE PROC SELECT_ACTIVE_CATEGORIES AS
+BEGIN
+	SELECT  C.Id, C.[Name], C.ShortName, C.CreatedDate, U.Username "Created By", C.UpdatedDate, U2.Username "Updated By",C2.[Name] "Company", C.[Status]
+	FROM Category "C"
+	LEFT JOIN Users "U" ON C.CreatedBy = U.Id
+	LEFT JOIN Users "U2" ON C.UpdatedBy = U2.Id 
+	LEFT JOIN Company "C2" ON C.CompanyId = C2.Id WHERE C.[Status] = 1;
+END
