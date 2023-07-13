7a0bdc35dde68f4dbde48cbe7913b8b1019036cd
DixitBhavyang <dixitbhavyang111@gmail.com>
2023-07-13 21:59:02 +0530

    Changed the Splashscreen Image

--- GSMS/SplashScreen.Designer.cs
+++ GSMS/SplashScreen.Designer.cs
@@ -29,9 +29,12 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(SplashScreen));
             this.progressBar1 = new System.Windows.Forms.ProgressBar();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.radPictureBox1 = new Telerik.WinControls.UI.RadPictureBox();
+            this.kryptonLabel1 = new ComponentFactory.Krypton.Toolkit.KryptonLabel();
+            ((System.ComponentModel.ISupportInitialize)(this.radPictureBox1)).BeginInit();
+            this.radPictureBox1.SuspendLayout();
             this.SuspendLayout();
             // 
             // progressBar1
@@ -47,20 +50,43 @@
             this.timer1.Interval = 20;
             this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
             // 
+            // radPictureBox1
+            // 
+            this.radPictureBox1.Controls.Add(this.kryptonLabel1);
+            this.radPictureBox1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.radPictureBox1.Image = global::GSMS.Properties.Resources.SplashScreenImage;
+            this.radPictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.radPictureBox1.Name = "radPictureBox1";
+            this.radPictureBox1.Size = new System.Drawing.Size(760, 461);
+            this.radPictureBox1.TabIndex = 2;
+            // 
+            // kryptonLabel1
+            // 
+            this.kryptonLabel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.kryptonLabel1.LabelStyle = ComponentFactory.Krypton.Toolkit.LabelStyle.TitlePanel;
+            this.kryptonLabel1.Location = new System.Drawing.Point(0, 0);
+            this.kryptonLabel1.Name = "kryptonLabel1";
+            this.kryptonLabel1.Size = new System.Drawing.Size(760, 29);
+            this.kryptonLabel1.TabIndex = 0;
+            this.kryptonLabel1.Values.Text = "Supermarket Management System";
+            // 
             // SplashScreen
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.ClientSize = new System.Drawing.Size(760, 472);
             this.ControlBox = false;
+            this.Controls.Add(this.radPictureBox1);
             this.Controls.Add(this.progressBar1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "SplashScreen";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "SplashScreen";
             this.Load += new System.EventHandler(this.SplashScreen_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.radPictureBox1)).EndInit();
+            this.radPictureBox1.ResumeLayout(false);
+            this.radPictureBox1.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -68,5 +94,7 @@
         #endregion
         private System.Windows.Forms.ProgressBar progressBar1;
         private System.Windows.Forms.Timer timer1;
+        private Telerik.WinControls.UI.RadPictureBox radPictureBox1;
+        private ComponentFactory.Krypton.Toolkit.KryptonLabel kryptonLabel1;
     }
 }
