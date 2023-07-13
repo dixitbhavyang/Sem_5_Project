99c6ef2c794a7946ead1b59e2ac853fab489d4f0
DixitBhavyang <dixitbhavyang111@gmail.com>
2023-07-13 22:22:11 +0530

    Update SELECT_COMPANIES.sql

--- SuperMarketDeskDB/dbo/Stored Procedures/SELECT_COMPANIES.sql
+++ SuperMarketDeskDB/dbo/Stored Procedures/SELECT_COMPANIES.sql
@@ -4,5 +4,5 @@ BEGIN
 SELECT  C.Id, C.[Name], C.ShortName, C.CreatedDate, U.Username "Created By", C.UpdatedDate, U2.Username "Updated By", C.[Address], C.[Status]
 FROM Company "C"
 LEFT JOIN Users "U" ON C.CreatedBy = U.Id
-LEFT JOIN Users "U2" ON C.UpdatedBy = U2.Id WHERE C.[Status]=1
+LEFT JOIN Users "U2" ON C.UpdatedBy = U2.Id
 END
