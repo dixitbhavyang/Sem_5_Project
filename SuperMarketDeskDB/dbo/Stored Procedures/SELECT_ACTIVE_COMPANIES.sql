55f0a36a373523ade11df4a0944b54c46ab83669
DixitBhavyang <dixitbhavyang111@gmail.com>
2023-07-13 22:21:46 +0530

    Create SELECT_ACTIVE_COMPANIES.sql

---
+++ SuperMarketDeskDB/dbo/Stored Procedures/SELECT_ACTIVE_COMPANIES.sql
@@ -0,0 +1,7 @@
+﻿CREATE PROC SELECT_ACTIVE_COMPANIES AS
+BEGIN
+	SELECT  C.Id, C.[Name], C.ShortName, C.CreatedDate, U.Username "Created By", C.UpdatedDate, U2.Username "Updated By", C.[Address], C.[Status]
+	FROM Company "C"
+	LEFT JOIN Users "U" ON C.CreatedBy = U.Id
+	LEFT JOIN Users "U2" ON C.UpdatedBy = U2.Id WHERE C.[Status]=1;
+END
