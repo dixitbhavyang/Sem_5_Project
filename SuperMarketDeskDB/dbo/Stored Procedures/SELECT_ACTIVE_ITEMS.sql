5b3a01d48d8b790343ebb23510ca4ae9a83af3a0
DixitBhavyang <dixitbhavyang111@gmail.com>
2023-07-13 22:21:52 +0530

    Create SELECT_ACTIVE_ITEMS.sql

---
+++ SuperMarketDeskDB/dbo/Stored Procedures/SELECT_ACTIVE_ITEMS.sql
@@ -0,0 +1,11 @@
+﻿CREATE PROC SELECT_ACTIVE_ITEMS AS
+BEGIN
+	SELECT  
+	I.Id, I.[Name], I.ShortName, I.Price, I.Discount, I.Tax, I.ExpiryDate, C.[Name] "Category", 
+	I.CreatedDate, U.[Username] "Created By", 
+	I.UpdatedDate, U2.[Username] "Updated By", I.[Status]
+	FROM Item "I"
+	LEFT JOIN Users "U" ON I.CreatedBy = U.Id
+	LEFT JOIN Users "U2" ON I.UpdatedBy = U2.Id
+	LEFT JOIN Category "C" ON I.CategoryId = C.Id WHERE I.[Status] = 1;
+END
