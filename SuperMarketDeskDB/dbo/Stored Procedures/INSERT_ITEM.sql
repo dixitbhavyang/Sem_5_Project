44ae66f95158ee0f355d388505600fab5063d10e
DixitBhavyang <dixitbhavyang111@gmail.com>
2023-07-13 22:21:36 +0530

    Create INSERT_ITEM.sql

---
+++ SuperMarketDeskDB/dbo/Stored Procedures/INSERT_ITEM.sql
@@ -0,0 +1,39 @@
+﻿CREATE PROCEDURE INSERT_ITEM 
+	@CATEGORYID INT,
+	@NAME VARCHAR(100), 
+	@SHORTNAME VARCHAR(10),
+	@PRICE DECIMAL(9,2),
+	@DISCOUNT DECIMAL(9,2),
+	@TAX DECIMAL(9,2),
+	@CREATEDDATE DATETIME,
+	@CREATEDBY INT,
+	@UPDATEDDATE DATETIME,
+	@UPDATEDBY INT,
+	@EXPIRYDATE DATETIME
+AS
+BEGIN
+	INSERT INTO [dbo].[Item]
+           ([CategoryId]
+           ,[Name]
+           ,[ShortName]
+           ,[Price]
+           ,[Discount]
+           ,[Tax]
+           ,[CreatedDate]
+           ,[CreatedBy]
+           ,[UpdatedDate]
+           ,[UpdatedBy]
+           ,[ExpiryDate])
+     VALUES
+           (@CATEGORYID
+		   ,@NAME
+		   ,@SHORTNAME
+		   ,@PRICE
+		   ,@DISCOUNT
+		   ,@TAX
+		   ,@CREATEDDATE
+		   ,@CREATEDBY
+		   ,@UPDATEDDATE
+		   ,@UPDATEDBY
+		   ,@EXPIRYDATE);
+END
