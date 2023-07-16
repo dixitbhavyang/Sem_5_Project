516f924717c961eb0668c8a9f60be2c98e7dffd2
DixitBhavyang <dixitbhavyang111@gmail.com>
2023-07-16 16:01:30 +0530

    Update Item.sql
    Added DiscountType and TaxType Columns

--- SuperMarketDeskDB/dbo/Tables/Item.sql
+++ SuperMarketDeskDB/dbo/Tables/Item.sql
@@ -1,22 +1,26 @@
 ﻿CREATE TABLE [dbo].[Item] (
-    [Id]          INT            IDENTITY (1, 1) NOT NULL,
-    [CategoryId]  INT            NOT NULL,
-    [Name]        VARCHAR (100)  NULL,
-    [ShortName]   VARCHAR (10)   NULL,
-    [Price]       DECIMAL (9, 2) NULL,
-    [Discount]    DECIMAL (9, 2) NULL,
-    [Tax]         DECIMAL (9, 2) NULL,
-    [CreatedDate] DATETIME       NULL,
-    [CreatedBy]   INT            NULL,
-    [UpdatedDate] DATETIME       NULL,
-    [UpdatedBy]   INT            NULL,
-    [Status]      BIT            CONSTRAINT [DF_Item_Status] DEFAULT ((1)) NULL,
-    [ExpiryDate]  DATETIME       NULL,
+    [Id]           INT            IDENTITY (1, 1) NOT NULL,
+    [CategoryId]   INT            NOT NULL,
+    [Name]         VARCHAR (100)  NULL,
+    [ShortName]    VARCHAR (10)   NULL,
+    [Price]        DECIMAL (9, 2) NULL,
+    [Discount]     DECIMAL (9, 2) NULL,
+    [DiscountType] BIT            NULL,
+    [Tax]          DECIMAL (9, 2) NULL,
+    [TaxType]      BIT            NULL,
+    [CreatedDate]  DATETIME       NULL,
+    [CreatedBy]    INT            NULL,
+    [UpdatedDate]  DATETIME       NULL,
+    [UpdatedBy]    INT            NULL,
+    [Status]       BIT            CONSTRAINT [DF_Item_Status] DEFAULT ((1)) NULL,
+    [ExpiryDate]   DATETIME       NULL,
     CONSTRAINT [PK_Item] PRIMARY KEY CLUSTERED ([Id] ASC),
-    FOREIGN KEY ([CreatedBy]) REFERENCES [dbo].[Users] ([Id]),
-    FOREIGN KEY ([UpdatedBy]) REFERENCES [dbo].[Users] ([Id]),
-    CONSTRAINT [FK__Item__CategoryId__3864608B] FOREIGN KEY ([CategoryId]) REFERENCES [dbo].[Category] ([Id])
+    CONSTRAINT [FK__Item__CategoryId__3864608B] FOREIGN KEY ([CategoryId]) REFERENCES [dbo].[Category] ([Id]),
+    CONSTRAINT [FK__Item__CreatedBy__44CA3770] FOREIGN KEY ([CreatedBy]) REFERENCES [dbo].[Users] ([Id]),
+    CONSTRAINT [FK__Item__UpdatedBy__45BE5BA9] FOREIGN KEY ([UpdatedBy]) REFERENCES [dbo].[Users] ([Id])
 );
+
+
 
 
 
