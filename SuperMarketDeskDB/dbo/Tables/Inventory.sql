9f8e673c48c2977c1c3b1a7d2e403ee2c45acdf0
DixitBhavyang <dixitbhavyang111@gmail.com>
2023-07-23 15:30:55 +0530

    Update Inventory.sql

--- SuperMarketDeskDB/dbo/Tables/Inventory.sql
+++ SuperMarketDeskDB/dbo/Tables/Inventory.sql
@@ -1,18 +1,26 @@
 ﻿CREATE TABLE [dbo].[Inventory] (
-    [Id]           INT            NOT NULL,
-    [ComapnyId]    INT            NULL,
+    [Id]           INT            IDENTITY (1, 1) NOT NULL,
+    [CompanyId]    INT            NULL,
     [CategoryId]   INT            NULL,
     [ItemId]       INT            NULL,
-    [ItemName]     VARCHAR (50)   NULL,
-    [Qauntity]     DECIMAL (9, 2) NULL,
+    [Quantity]     DECIMAL (9, 2) NULL,
     [Unit]         INT            NULL,
     [CreatedDate]  DATETIME       NULL,
-    [Status]       BIT            CONSTRAINT [DF_Inventory_Status] DEFAULT ((1)) NULL,
+    [CreatedBy]    INT            NULL,
+    [LastUpdated]  DATETIME       NULL,
+    [UpdatedBy]    INT            NULL,
     [MinimumStock] INT            NULL,
     [MaximumStock] INT            NULL,
-    [LastUpdated]  DATETIME       NULL,
-    CONSTRAINT [PK_Inventory] PRIMARY KEY CLUSTERED ([Id] ASC)
+    [Status]       BIT            CONSTRAINT [DF_Inventory_Status] DEFAULT ((1)) NULL,
+    CONSTRAINT [PK_Inventory] PRIMARY KEY CLUSTERED ([Id] ASC),
+    FOREIGN KEY ([CategoryId]) REFERENCES [dbo].[Category] ([Id]),
+    FOREIGN KEY ([CompanyId]) REFERENCES [dbo].[Company] ([Id]),
+    FOREIGN KEY ([CreatedBy]) REFERENCES [dbo].[Users] ([Id]),
+    FOREIGN KEY ([UpdatedBy]) REFERENCES [dbo].[Users] ([Id]),
+    CONSTRAINT [FK__Inventory__ItemI__793DFFAF] FOREIGN KEY ([ItemId]) REFERENCES [dbo].[Item] ([Id])
 );
+
+
 
 
 
