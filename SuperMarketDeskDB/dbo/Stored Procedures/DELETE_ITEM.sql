43cd5ae8a27a1441e7c3e90edcb0e6476739248b
DixitBhavyang <dixitbhavyang111@gmail.com>
2023-07-23 15:31:06 +0530

    Update DELETE_ITEM.sql

--- SuperMarketDeskDB/dbo/Stored Procedures/DELETE_ITEM.sql
+++ SuperMarketDeskDB/dbo/Stored Procedures/DELETE_ITEM.sql
@@ -1,6 +1,14 @@
-﻿CREATE PROC DELETE_ITEM 
+﻿CREATE PROC [dbo].[DELETE_ITEM] 
 	@ID INT
 AS
 BEGIN
-	UPDATE Item SET [Status] = 0 WHERE Id = @ID;
+	IF EXISTS (SELECT * FROM Inventory WHERE ItemId = @ID AND [Status] = 1)
+    BEGIN
+        -- Return the existing record
+        SELECT I.Unit FROM Inventory "I" WHERE I.ItemId = @ID;
+    END
+	ELSE
+	BEGIN
+		UPDATE Item SET [Status] = 0 WHERE Id = @ID;
+	END
 END
