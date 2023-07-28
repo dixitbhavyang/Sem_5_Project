afb542b8f130a8b9d6f3023f72a6f9ecb901c327
DixitBhavyang <dixitbhavyang111@gmail.com>
2023-07-28 13:59:11 +0530

    Create Department.sql

---
+++ SuperMarketDeskDB/dbo/Tables/Department.sql
@@ -0,0 +1,17 @@
+﻿CREATE TABLE [dbo].[Department] (
+    [Id]          INT          IDENTITY (1, 1) NOT NULL,
+    [Name]        VARCHAR (50) NULL,
+    [ShortName]   VARCHAR (10) NULL,
+    [Phone]       VARCHAR (10) NULL,
+    [Description] TEXT         NULL,
+    [CreatedDate] DATETIME     NULL,
+    [CreatedBy]   INT          NULL,
+    [LastUpdated] DATETIME     NULL,
+    [UpdatedBy]   INT          NULL,
+    [Status]      BIT          CONSTRAINT [DF_Department_Status] DEFAULT ((1)) NULL,
+    CONSTRAINT [PK_Department] PRIMARY KEY CLUSTERED ([Id] ASC),
+    FOREIGN KEY ([UpdatedBy]) REFERENCES [dbo].[Users] ([Id]),
+    FOREIGN KEY ([UpdatedBy]) REFERENCES [dbo].[Users] ([Id]),
+    CONSTRAINT [FK__Departmen__Creat__4E1E9780] FOREIGN KEY ([CreatedBy]) REFERENCES [dbo].[Users] ([Id])
+);
+
